fbf02ba0835ff344360050e2e8189fa3c4ffc93f
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-09-02 10:37:46 -0300

    feat: import and use navbar and footer components

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import { Home } from './pages/Home/Home';
 import { About } from './pages/About/About';
+import { Navbar, Footer } from './components';
 
 export const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </div>
+        <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
