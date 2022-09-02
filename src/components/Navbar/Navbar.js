b0b9510b0c988e121e8e45420c2478af668ba996
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-09-02 10:35:28 -0300

    feat: add navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import { NavLink } from 'react-router-dom';
+
+import styles from './Navbar.module.css';
+
+export const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <NavLink to="/" className={styles.brand}>
+        Mini <span>Blob</span>
+      </NavLink>
+      <ul className={styles.links_list}>
+        <li>
+          <NavLink to="/" className={({isActive}) => (isActive ? styles.active : '')}>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" className={({isActive}) => (isActive ? styles.active : '')}>About</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
