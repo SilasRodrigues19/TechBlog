1be79bc3a3cfc0215daa00f50013f1450f96d704
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-09-02 09:55:28 -0300

    feat: add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import { Home } from './pages/Home/Home';
+import { About } from './pages/About/About';
+
 export const App = () => {
   return (
     <div className="App">
-      <h1>Miniblog</h1>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
