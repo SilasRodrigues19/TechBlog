a6476e45ee900f699941400ec4358918891f6921
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-09-07 10:45:24 -0300

    feat: add login and register in the navigation

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,17 +5,43 @@ import styles from './Navbar.module.scss';
 export const Navbar = () => {
   return (
     <nav className={styles.navbar}>
-      <NavLink to="/">
+      <NavLink to='/'>
         Mini <span>Blob</span>
       </NavLink>
       <ul className={styles.links_list}>
         <li>
-          <NavLink to="/" className={({isActive}) => (isActive ? styles.active : '')}>Home</NavLink>
+          <NavLink
+            to='/'
+            className={({ isActive }) => (isActive ? styles.active : '')}
+          >
+            Inicio
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/about" className={({isActive}) => (isActive ? styles.active : '')}>About</NavLink>
+          <NavLink
+            to='/login'
+            className={({ isActive }) => (isActive ? styles.active : '')}
+          >
+            Entrar
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/register'
+            className={({ isActive }) => (isActive ? styles.active : '')}
+          >
+            Cadastrar
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/about'
+            className={({ isActive }) => (isActive ? styles.active : '')}
+          >
+            Sobre
+          </NavLink>
         </li>
       </ul>
     </nav>
   );
-}
+};
