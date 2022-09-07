af07aba9a1ec4869e2b0702cdf25a22969f12ffc
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-09-07 10:40:04 -0300

    refactor: change footer to portuguese

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import styles from './Footer.module.scss';
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <h3>Write about what you're interested in!</h3>
+      <h3>Escreva sobre o que você está interessado!</h3>
       <p>Mini Blog &copy; 2022</p>
     </footer>
   );
