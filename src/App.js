28085471446e5bbff7a41f2877f4f096c04d410b
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-09-07 10:44:52 -0300

    feat: add login and register routes

--- src/App.js
+++ src/App.js
@@ -2,24 +2,24 @@ import './App.scss';
 
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-import { Home } from './pages/Home/Home';
-import { About } from './pages/About/About';
+import { Home, About, Login, Register } from './pages';
 import { Navbar, Footer } from './components';
 
 export const App = () => {
   return (
-    <div className="App">
+    <div className='App'>
       <BrowserRouter>
         <Navbar />
-          <div className="container">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-            </Routes>
-          </div>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
         <Footer />
       </BrowserRouter>
     </div>
   );
-}
-
+};
