4bbcc6b189cf49e8f9e893bf8732c3ec2acc4bb2
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-09-12 16:56:26 -0300

    refactor: add exports for new added pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,5 @@ export * from './About/About';
 export * from './Home/Home';
 export * from './Login/Login';
 export * from './Register/Register';
+export * from './Dashboard/Dashboard';
+export * from './CreatePost/CreatePost';
