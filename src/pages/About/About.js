ee787c6eab82685d7e32788a1533e9a75dcde2f0
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-09-12 16:57:13 -0300

    feat: create content for about page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,20 @@
 import styles from './About.module.scss';
 
+import { Link } from 'react-router-dom';
+
 export const About = () => {
   return (
-    <>
-      <h1>About</h1>
-    </>
+    <section className={styles.about}>
+      <h2>
+        Sobre o Mini <span>Blog</span>
+      </h2>
+      <p>
+        Este projeto consiste em um blog feito com React para construção da
+        interface do usuário e serviços do Firebase utilizados no Back End
+      </p>
+      <Link className='btn' to='/post/create'>
+        Criar post
+      </Link>
+    </section>
   );
 };
