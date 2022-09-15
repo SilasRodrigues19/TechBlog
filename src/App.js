540d4447d976b7c2017146c9b5801f4360fb28b7
SilasRodrigues19 <silasrodrigues.fatec@gmail.com>
2022-09-14 21:25:34 -0300

    refactor: remove app.scss

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import './App.scss';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { onAuthStateChanged } from "firebase/auth";
+import { AuthProvider } from "./context/AuthContext";
 
-import { onAuthStateChanged } from 'firebase/auth';
-import { AuthProvider } from './context/AuthContext';
+import { useState, useEffect } from "react";
+import { useAuth } from "./hooks/useAuth";
 
-import { useState, useEffect } from 'react';
-import { useAuth } from './hooks/useAuth';
-
-import { Home, About, Login, Register, Dashboard, CreatePost } from './pages';
-import { Navbar, Footer } from './components';
+import { Home, About, Login, Register, Dashboard, CreatePost } from "./pages";
+import { Navbar, Footer } from "./components";
 
 export const App = () => {
   const [user, setUser] = useState(undefined);
@@ -26,31 +24,31 @@ export const App = () => {
   if (loadingUser) return <p>Carregando...</p>;
 
   return (
-    <div className='App'>
+    <div className="App">
       <AuthProvider value={{ user }}>
         <BrowserRouter>
           <Navbar />
-          <div className='container'>
+          <div className="container">
             <Routes>
-              <Route exact path='/' element={<Home />} />
-              <Route path='/about' element={<About />} />
+              <Route exact path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
               <Route
-                path='/login'
-                element={!user ? <Login /> : <Navigate to='/' />}
+                path="/login"
+                element={!user ? <Login /> : <Navigate to="/" />}
               />
               <Route
-                path='/register'
-                element={!user ? <Register /> : <Navigate to='/' />}
+                path="/register"
+                element={!user ? <Register /> : <Navigate to="/" />}
               />
               <Route
-                path='/dashboard'
-                element={user ? <Dashboard /> : <Navigate to='/' />}
+                path="/dashboard"
+                element={user ? <Dashboard /> : <Navigate to="/" />}
               />
               <Route
-                path='/post/create'
-                element={user ? <CreatePost /> : <Navigate to='/' />}
+                path="/post/create"
+                element={user ? <CreatePost /> : <Navigate to="/" />}
               />
-              <Route path='*' element={<h1>404 (Ainda não implementada)</h1>} />
+              <Route path="*" element={<h1>404 (Ainda não implementada)</h1>} />
             </Routes>
           </div>
           <Footer />
