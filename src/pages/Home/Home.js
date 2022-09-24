604a2f63cad3dd8fc11385a6fe2a4eade4c14b92
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-09-24 19:57:20 -0300

    feat: add initial structure in the home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,43 @@
 import styles from './Home.module.scss';
 
+import { useNavigate, Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Icon } from '@iconify/react';
+
 export const Home = () => {
+
+  const [query, setQuery] = useState('');
+  const [posts] = useState([]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  }
+
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <div className={styles.home}>
+      <h1>Veja os nossos posts mais recentes</h1>
+      <form onSubmit={handleSubmit} className={styles.searchForm}>
+        <input
+          type='text'
+          placeholder='Busque por algum termo...'
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className='btn btn-dark'>Pesquisar</button>
+      </form>
+      <div>
+        <h1>Posts...</h1>
+        {posts && posts.length === 0 && (
+          <div className={styles.hasNoPosts}>
+            <div className='error'>
+              <p>
+                <Icon className='dangerIcon' icon='jam:triangle-danger-f' />
+                Não existe nada publicado.
+              </p>
+            </div>
+            <Link to='/posts/create' className='btn'>Criar novo post</Link>
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
