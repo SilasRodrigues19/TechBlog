0fd29e792f70b658e74ffff62bc5474cf61edc27
SilasRodrigues19 <silasrodrigues.fatec@gmail.com>
2022-09-26 03:41:55 -0300

    feat: finish home page fetching the posts

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,39 +2,43 @@ import styles from './Home.module.scss';
 
 import { useNavigate, Link } from 'react-router-dom';
 import { useState } from 'react';
+import { useFetchDocuments } from '../../hooks/useFetchDocuments';
 import { Icon } from '@iconify/react';
+import { PostDetail } from '../../components/PostDetail/PostDetail';
 
 export const Home = () => {
-
   const [query, setQuery] = useState('');
-  const [posts] = useState([]);
+  const { documents: posts, loading } = useFetchDocuments('posts');
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-  }
+  };
 
   return (
     <div className={styles.home}>
       <h1>Veja os nossos posts mais recentes</h1>
       <form onSubmit={handleSubmit} className={styles.searchForm}>
         <input
-          type='text'
-          placeholder='Busque por algum termo...'
+          type="text"
+          placeholder="Busque por algum termo..."
           onChange={(e) => setQuery(e.target.value)}
         />
-        <button className='btn btn-dark'>Pesquisar</button>
+        <button className="btn btn-dark">Pesquisar</button>
       </form>
       <div>
-        <h1>Posts...</h1>
+        {loading && <p>Carregando...</p>}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
         {posts && posts.length === 0 && (
           <div className={styles.hasNoPosts}>
-            <div className='error'>
+            <div className="error">
               <p>
-                <Icon className='dangerIcon' icon='jam:triangle-danger-f' />
+                <Icon className="dangerIcon" icon="jam:triangle-danger-f" />
                 Não existe nada publicado.
               </p>
             </div>
-            <Link to='/post/create' className='btn'>Criar novo post</Link>
+            <Link to="/post/create" className="btn">
+              Criar novo post
+            </Link>
           </div>
         )}
       </div>
