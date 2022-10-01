5b56a4d4d0009c987d2f827311f315f46bf0ed87
SilasRodrigues19 <silasrodrigues.fatec@gmail.com>
2022-10-01 11:38:43 -0300

    feat: update query adding filter

--- src/hooks/useFetchDocuments.js
+++ src/hooks/useFetchDocuments.js
@@ -27,7 +27,15 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
       try {
         let q;
 
-        q = await query(collectionRef, orderBy('createdAt', 'desc'));
+        if (search) {
+          q = await query(
+            collectionRef,
+            where('tagsArray', 'array-contains-any', [search]),
+            orderBy('createdAt', 'desc')
+          );
+        } else {
+          q = await query(collectionRef, orderBy('createdAt', 'desc'));
+        }
 
         await onSnapshot(q, (querySnapshot) => {
           setDocuments(
