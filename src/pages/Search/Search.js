bca796c3a2be9cbd86c6007d5a8936fb0ef37e07
SilasRodrigues19 <silasrodrigues.fatec@gmail.com>
2022-10-01 09:18:39 -0300

    feat: add search page

---
+++ src/pages/Search/Search.js
@@ -0,0 +1,14 @@
+import { useFetchDocuments } from '../../hooks/useFetchDocuments';
+import { useQuery } from '../../hooks/useQuery';
+
+export const Search = () => {
+  const query = useQuery();
+  const search = query.get('q');
+
+  return (
+    <div>
+      <h2>Search</h2>
+      <p>{search}</p>
+    </div>
+  );
+};
