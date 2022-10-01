80f89dc59f9390f9f8455b90742146531bb0e990
SilasRodrigues19 <silasrodrigues.fatec@gmail.com>
2022-10-01 11:40:27 -0300

    feat: finish search page listing the filtered posts

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,14 +1,39 @@
 import { useFetchDocuments } from '../../hooks/useFetchDocuments';
 import { useQuery } from '../../hooks/useQuery';
 
+import { PostDetail } from '../../components/PostDetail/PostDetail';
+
+import { Link } from 'react-router-dom';
+
+import styles from './Search.module.scss';
+
 export const Search = () => {
   const query = useQuery();
   const search = query.get('q');
 
+  const { documents: posts } = useFetchDocuments('posts', search);
+
   return (
-    <div>
-      <h2>Search</h2>
-      <p>{search}</p>
+    <div className={styles.searchContainer}>
+      {posts && posts.length === 0 ? (
+        <>
+          <p>
+            Não foram encontrados posts com o termo{' '}
+            <span className={styles.term}>{search}</span>
+          </p>
+          <Link to="/" className="btn btn-dark">
+            Voltar
+          </Link>
+        </>
+      ) : (
+        <h2>
+          Resultados encontrados para{' '}
+          <span className={styles.term}>{search}</span>
+        </h2>
+      )}
+      <div>
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
+      </div>
     </div>
   );
 };
