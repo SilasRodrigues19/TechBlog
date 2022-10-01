03f7020847fa9919904a3b33f4d1dee64446e4a4
SilasRodrigues19 <silasrodrigues.fatec@gmail.com>
2022-10-01 12:15:06 -0300

    feat: dynamically redirect page based to auth state

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -2,7 +2,11 @@ import styles from './About.module.scss';
 
 import { Link } from 'react-router-dom';
 
+import { useAuthValue } from '../../context/AuthContext';
+
 export const About = () => {
+  const { user } = useAuthValue();
+
   return (
     <section className={styles.about}>
       <h2>
@@ -12,7 +16,7 @@ export const About = () => {
         Este projeto consiste em um blog feito com React para construção da
         interface do usuário e serviços do Firebase utilizados no Back End
       </p>
-      <Link className="btn" to="/post/create">
+      <Link className="btn" to={user ? '/post/create' : '/register'}>
         Criar post
       </Link>
     </section>
