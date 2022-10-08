2cd3582d2a2c6cd9ac029453831df519fc07b111
SilasRodrigues19 <silasrodrigues.fatec@gmail.com>
2022-10-08 13:54:14 -0300

    refactor: export hooks in index file
    - export all hooks to be imported more easily in the project

---
+++ src/hooks/index.js
@@ -0,0 +1,6 @@
+export * from './useAuth';
+export * from './useFetchDocument';
+export * from './useFetchDocuments';
+export * from './useInsertDocument';
+export * from './useQuery';
+export * from './useTitle';
