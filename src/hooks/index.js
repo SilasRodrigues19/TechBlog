76ae6387fbb1de14410fb32f6ffe187cd413e495
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-06-01 21:47:31 -0300

    feat: create hook to edit post

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -3,5 +3,6 @@ export * from './useFetchDocument';
 export * from './useFetchDocuments';
 export * from './useInsertDocument';
 export * from './useDeleteDocument';
+export * from './useUpdateDocument';
 export * from './useQuery';
 export * from './useTitle';
