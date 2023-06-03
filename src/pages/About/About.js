c4e26f31c08ab5ab6642c3c8a885a3152b304a41
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-06-02 21:26:34 -0300

    refactor: restyle about page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,5 +1,3 @@
-import styles from './About.module.scss';
-
 import { Link } from 'react-router-dom';
 
 import { useAuthValue } from '../../contexts/AuthContext';
@@ -11,17 +9,22 @@ export const About = () => {
   const { user } = useAuthValue();
 
   return (
-    <section className={styles.about}>
-      <h2>
-        Sobre o Tech <span>Blog</span>
-      </h2>
-      <p>
-        Este projeto consiste em um blog feito com React para construção da
-        interface do usuário e serviços do Firebase utilizados no Back End
-      </p>
-      <Link className="btn" to={user ? '/post/create' : '/register'}>
-        Criar post
-      </Link>
+    <section className='bg-gray-100 py-10 px-6'>
+      <div className='max-w-3xl mx-auto text-center'>
+        <h2 className='text-3xl text-gray-700 font-bold mb-8'>
+          Sobre o <span className='text-gray-500'>Tech Blog</span>
+        </h2>
+        <p className='text-lg text-gray-500 mb-8'>
+          Este projeto consiste em um blog feito com React para construção da
+          interface do usuário e serviços do Firebase utilizados no Back End
+        </p>
+        <Link
+          to={user ? '/post/create' : '/register'}
+          className='inline-block bg-gray-700 hover:bg-gray-600 hover:text-white text-white px-8 py-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50'
+        >
+          Criar post
+        </Link>
+      </div>
     </section>
   );
 };
