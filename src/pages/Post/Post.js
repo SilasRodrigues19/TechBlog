31b6473c18be4d199f842e933ebb0ede8011cd5b
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-06-02 21:28:08 -0300

    refactor: restyle post page

--- src/pages/Post/Post.js
+++ src/pages/Post/Post.js
@@ -1,5 +1,3 @@
-import styles from './Post.module.scss';
-
 import { Link, useParams } from 'react-router-dom';
 
 import { useFetchDocument, useTitle } from '../../hooks';
@@ -13,26 +11,40 @@ export const Post = () => {
   useTitle('Tech Blog | Ver post');
 
   return (
-    <div className={styles.postContainer}>
+    <div className='max-w-screen-lg mx-auto px-4'>
       {loading && <Loader />}
       {post && (
-        <>
-          <h1>{post.title}</h1>
-          <img src={post.image} alt={post.title} loading="lazy" />
-          <p className={post.body.length > 150 ? styles.content : ''}>{post.body}</p>
-          <h3>Categorias associadas a este post:</h3>
-          <div className={styles.tags}>
+        <div className='flex justify-center flex-col items-center w-full'>
+          <h1 className='text-3xl text-gray-700 font-bold text-center mb-8'>
+            {post.title}
+          </h1>
+          <img
+            src={post.image}
+            alt={post.title}
+            loading='lazy'
+            className='w-full h-[10rem] mb-4 rounded-lg object-contain'
+          />
+          <p className='text-gray-800 mb-4'>{post.body}</p>
+          <h3 className='text-lg font-bold mb-2'>
+            Categorias associadas a este post:
+          </h3>
+          <div className='flex flex-wrap'>
             {post.tagsArray.map((tag) => (
-              <p key={tag}>
-                <span>&nbsp;#</span>
-                {tag}
+              <p
+                key={tag}
+                className='bg-gray-200 text-gray-800 px-3 py-1 rounded-full mr-2 my-4'
+              >
+                #{tag}
               </p>
             ))}
           </div>
-          <Link to="/" className="btn btn-dark">
+          <Link
+            to='/'
+            className='bg-gray-700 hover:bg-gray-600 text-white px-8 py-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50 hover:text-white'
+          >
             Voltar
           </Link>
-        </>
+        </div>
       )}
     </div>
   );
