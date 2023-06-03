1d67e51d9c28f5634e73fb2379cfa14c3c5e87d1
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-06-02 21:25:59 -0300

    feat: add menu mobile and restyle navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,88 +1,188 @@
 import { NavLink } from 'react-router-dom';
-
 import { useAuth } from '../../hooks';
-
 import { useAuthValue } from '../../contexts/AuthContext';
-
-import styles from './Navbar.module.scss';
+import { Icon } from '@iconify/react';
+import { useState } from 'react';
 
 export const Navbar = () => {
   const { user } = useAuthValue();
   const { logout } = useAuth();
 
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => setMenuOpen(!menuOpen);
+
   return (
-    <nav className={styles.navbar}>
-      <NavLink to="/" className={styles.brand}>
-        Tech <span>Blog</span>
-      </NavLink>
-      <ul className={styles.links_list}>
-        <li>
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? styles.active : '')}
-          >
-            Inicio
-          </NavLink>
-        </li>
-
-        {!user && (
-          <>
-            <li>
-              <NavLink
-                to="/login"
-                className={({ isActive }) => (isActive ? styles.active : '')}
-              >
-                Entrar
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/register"
-                className={({ isActive }) =>
-                  isActive ? styles.active : styles.signup
-                }
-              >
-                Cadastrar
-              </NavLink>
-            </li>
-          </>
+    <nav className='bg-gray-800'>
+      <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+        <div className='flex items-center justify-between h-16'>
+          <div className='flex items-center'>
+            <NavLink
+              to='/'
+              className='text-white font-bold text-lg hover:text-white'
+            >
+              Tech <span>Blog</span>
+            </NavLink>
+          </div>
+          <div className='hidden md:block'>
+            <ul className='flex space-x-4'>
+              <li>
+                <NavLink
+                  to='/'
+                  exact
+                  className='inline-block text-gray-300 hover:text-white px-3 py-2 rounded-md font-medium focus:text-white active:bg-gray-900'
+                >
+                  Início
+                </NavLink>
+              </li>
+              {!user && (
+                <>
+                  <li>
+                    <NavLink
+                      to='/login'
+                      className='inline-block text-gray-300 hover:text-white px-3 py-2 rounded-md font-medium focus:text-white active:bg-gray-900'
+                    >
+                      Entrar
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to='/register'
+                      className='inline-block text-gray-300 hover:text-white px-3 py-2 rounded-md font-medium focus:text-white active:bg-gray-900'
+                    >
+                      Cadastrar
+                    </NavLink>
+                  </li>
+                </>
+              )}
+              {user && (
+                <>
+                  <li>
+                    <NavLink
+                      to='/dashboard'
+                      className='inline-block text-gray-300 hover:text-white px-3 py-2 rounded-md font-medium focus:text-white active:bg-gray-900'
+                    >
+                      Painel
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to='/post/create'
+                      className='inline-block text-gray-300 hover:text-white px-3 py-2 rounded-md font-medium focus:text-white active:bg-gray-900'
+                    >
+                      Criar post
+                    </NavLink>
+                  </li>
+                </>
+              )}
+              <li>
+                <NavLink
+                  to='/about'
+                  className='inline-block text-gray-300 hover:text-white px-3 py-2 rounded-md font-medium focus:text-white active:bg-gray-900'
+                >
+                  Sobre
+                </NavLink>
+              </li>
+              {user && (
+                <li>
+                  <button
+                    onClick={logout}
+                    className='text-gray-300 hover:text-white px-3 py-2 rounded-md font-medium focus:text-white active:bg-gray-900'
+                  >
+                    Sair
+                  </button>
+                </li>
+              )}
+            </ul>
+          </div>
+          <div className='md:hidden'>
+            <button
+              onClick={handleMenuToggle}
+              className='text-gray-300 hover:text-white px-2 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'
+            >
+              {menuOpen ? (
+                <Icon icon='iconamoon:close-duotone' className='h-6 w-6' />
+              ) : (
+                <Icon icon='ion:menu' className='h-6 w-6' />
+              )}
+            </button>
+          </div>
+        </div>
+        {menuOpen && (
+          <div className='md:hidden'>
+            <ul className='mt-2 space-y-2'>
+              <li>
+                <NavLink
+                  to='/'
+                  className='text-gray-300 hover:text-white px-3 py-2 rounded-md font-medium focus:text-white active:bg-gray-900'
+                >
+                  Inicio
+                </NavLink>
+              </li>
+              {!user && (
+                <>
+                  <li>
+                    <NavLink
+                      to='/login'
+                      className='text-gray-300 hover:text-white px-3 py-2 rounded-md font-medium focus:text-white active:bg-gray-900'
+                    >
+                      Entrar
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to='/register'
+                      className='text-gray-300 hover:text-white px-3 py-2 rounded-md font-medium focus:text-white active:bg-gray-900'
+                    >
+                      Cadastrar
+                    </NavLink>
+                  </li>
+                </>
+              )}
+              {user && (
+                <>
+                  <li>
+                    <NavLink
+                      to='/dashboard'
+                      className='text-gray-300 hover:text-white px-3 py-2 rounded-md font-medium focus:text-white active:bg-gray-900'
+                    >
+                      Painel
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to='/post/create'
+                      className='text-gray-300 hover:text-white px-3 py-2 rounded-md font-medium focus:text-white active:bg-gray-900'
+                    >
+                      Criar post
+                    </NavLink>
+                  </li>
+                </>
+              )}
+              <li>
+                <NavLink
+                  to='/about'
+                  className='text-gray-300 hover:text-white px-3 py-2 rounded-md font-medium focus:text-white active:bg-gray-900'
+                >
+                  Sobre
+                </NavLink>
+              </li>
+              {user && (
+                <li>
+                  <button
+                    onClick={logout}
+                    className='text-gray-300 hover:text-white px-3 py-2 rounded-md font-medium focus:text-white active:bg-gray-900'
+                  >
+                    Sair
+                  </button>
+                </li>
+              )}
+            </ul>
+          </div>
         )}
-
-        {user && (
-          <>
-            <li>
-              <NavLink
-                to="/dashboard"
-                className={({ isActive }) => (isActive ? styles.active : '')}
-              >
-                Painel
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/post/create"
-                className={({ isActive }) => (isActive ? styles.active : '')}
-              >
-                Criar post
-              </NavLink>
-            </li>
-          </>
-        )}
-
-        <li>
-          <NavLink
-            to="/about"
-            className={({ isActive }) => (isActive ? styles.active : '')}
-          >
-            Sobre
-          </NavLink>
-        </li>
-        {user && (
-          <li>
-            <button onClick={logout}>Sair</button>
-          </li>
-        )}
-      </ul>
+      </div>
     </nav>
   );
 };
+
+         
