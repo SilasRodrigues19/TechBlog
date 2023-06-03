983d00ee5bbad7116e67c4c91e8e9677916a8c15
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-06-02 21:25:35 -0300

    refactor: restyle loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,15 +1,13 @@
-import styles from './Loader.module.scss';
-
 import { Icon } from '@iconify/react';
 
 export const Loader = () => {
   return (
-    <div className={styles.wrapper}>
+    <div className='flex items-center justify-center fixed top-0 left-0 w-full h-full bg-black bg-opacity-75'>
       <Icon
-        icon="line-md:loading-loop"
-        color="white"
-        width="150"
-        height="150"
+        icon='line-md:loading-loop'
+        color='white'
+        width='150'
+        height='150'
       />
     </div>
   );
