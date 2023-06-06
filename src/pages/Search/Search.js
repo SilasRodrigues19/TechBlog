350a8e5a3dc88fa6d49f1026b9a6b3165bd654f3
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-06-05 21:18:13 -0300

    refactor: improve edit post styling

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -13,23 +13,29 @@ export const Search = () => {
   const { documents: posts } = useFetchDocuments('posts', search);
 
   return (
-    <div>
+    <div className='max-w-screen-lg mx-auto px-4'>
       {posts && posts.length === 0 ? (
-        <>
-          <p>Não foram encontrados posts com o termo
-            <p>{search}</p>
-          </p>
-          <Link to="/" className="btn btn-dark">
+        <div className='flex justify-center flex-col items-center w-full'>
+          <h1 className='text-3xl text-gray-700 font-bold text-center mb-8'>
+            Não foram encontrados posts com o termo
+            <p className='font-bold text-gray-500'>{search}</p>
+          </h1>
+          <Link
+            to='/'
+            className='inline-block bg-gray-700 hover:bg-gray-600 hover:text-white text-white px-8 py-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50'
+          >
             Voltar
           </Link>
-        </>
+        </div>
       ) : (
-        <div>
-          <h2>
-            Resultados encontrados para{' '}
-            <span>{search}</span>
-          </h2>
-          <Link to="/" className="btn btn-dark">
+        <div className='flex justify-center flex-col items-center w-full'>
+          <h1 className='text-3xl text-gray-700 font-bold text-center mb-8'>
+            Resultados encontrados para <span>{search}</span>
+          </h1>
+          <Link
+            to='/'
+            className='inline-block bg-gray-700 hover:bg-gray-600 hover:text-white text-white px-8 py-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50'
+          >
             Voltar
           </Link>
         </div>
