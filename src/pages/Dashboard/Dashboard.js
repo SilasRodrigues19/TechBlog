576ad796a9b3ce4460025bda710508e7c23fca89
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-06-05 21:18:38 -0300

    refactor: improve dashboard validation to show the posts

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -18,57 +18,65 @@ export const Dashboard = () => {
   
   if (loading) return <p>Carregando...</p>
 
+
   return (
     <div className='max-w-screen-lg mx-auto px-4'>
       <h2 className='text-3xl text-gray-700 font-bold text-center my-4'>
         Dashboard
       </h2>
-      <p className='text-2xl text-gray-500 text-center mb-8'>
-        Gerencie os seus posts
-      </p>
 
       {posts && posts.length === 0 ? (
-        <div className='border border-gray-200 bg-white bg-opacity-50 rounded p-4 mb-6'>
-          <p className='text-gray-500 mb-2'>Não foram encontrados posts</p>
-          <Link to='/post/create' className='btn btn-primary text-sm px-4 py-2'>
-            Criar primeiro post
-          </Link>
-        </div>
+        <>
+          <p className='text-2xl text-gray-500 text-center mb-8'>
+            Você ainda não possui nenhuma postagem publicada
+          </p>
+          <div className='border border-gray-200 bg-white bg-opacity-50 rounded p-4 mb-6'>
+            <p className='text-gray-500 mb-2'>Não foram encontrados posts</p>
+            <Link to='/post/create' className='btn btn-primary text-sm px-4 py-2'>
+              Criar primeiro post
+            </Link>
+          </div>
+        </>
       ) : (
-        <div>
-          {posts &&
-            posts.map(({ id, title }) => (
-              <div
-                key={id}
-                className='flex justify-between items-center text-gray-400 border-2 border-gray-100 bg-white bg-opacity-50 rounded-lg p-4 mb-4 shadow-lg'
-              >
-                <p className='text-gray-400 font-bold text-lg'>{title}</p>
-                <div className='flex space-x-4'>
-                  <Link
-                    to={`/post/${id}`}
-                    className='text-sm px-4 py-2 text-gray-400 hover:text-gray-500'
-                  >
-                    <Icon icon='ph:eye-fill' className='h-6 w-6' />
-                  </Link>
-                  <Link
-                    to={`/post/edit/${id}`}
-                    className='text-sm px-4 py-2 text-gray-400 hover:text-gray-500'
-                  >
-                    <Icon
-                      icon='material-symbols:edit-rounded'
-                      className='h-6 w-6'
-                    />
-                  </Link>
-                  <button
-                    onClick={() => deleteDocument(id)}
-                    className='inline-block text-sm px-4 py-2 text-gray-400 hover:text-gray-500'
-                  >
-                    <Icon icon='ic:baseline-delete' className='h-6 w-6' />
-                  </button>
+        <>
+          <p className='text-2xl text-gray-500 text-center mb-8'>
+            Gerencie os seus posts
+          </p>
+          <div>
+            {posts &&
+              posts.map(({ id, title }) => (
+                <div
+                  key={id}
+                  className='flex justify-between items-center text-gray-400 border-2 border-gray-100 bg-white bg-opacity-50 rounded-lg p-4 mb-4 shadow-lg'
+                >
+                  <p className='text-gray-400 font-bold text-lg'>{title}</p>
+                  <div className='flex space-x-4'>
+                    <Link
+                      to={`/post/${id}`}
+                      className='text-sm px-4 py-2 text-gray-400 hover:text-gray-500'
+                    >
+                      <Icon icon='ph:eye-fill' className='h-6 w-6' />
+                    </Link>
+                    <Link
+                      to={`/post/edit/${id}`}
+                      className='text-sm px-4 py-2 text-gray-400 hover:text-gray-500'
+                    >
+                      <Icon
+                        icon='material-symbols:edit-rounded'
+                        className='h-6 w-6'
+                      />
+                    </Link>
+                    <button
+                      onClick={() => deleteDocument(id)}
+                      className='inline-block text-sm px-4 py-2 text-gray-400 hover:text-gray-500'
+                    >
+                      <Icon icon='ic:baseline-delete' className='h-6 w-6' />
+                    </button>
+                  </div>
                 </div>
-              </div>
-            ))}
-        </div>
+              ))}
+          </div>
+        </>
       )}
     </div>
   );
